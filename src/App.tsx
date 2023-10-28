bd5459fcdc3dc58a6ffdf8bd336e4c22565b79d4
Christopher Forti <christopher.forti.79@gmail.com>
2023-10-28 14:13:38 -0400

    changed button controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg' 
 import './App.css'
 
 function App() {
@@ -15,14 +15,17 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+          </button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+             press button
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
