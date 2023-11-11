9dc54e05d30628fc30c8494e53795b4f0d20dc22
Christopher Forti <christopher.forti.79@gmail.com>
2023-11-11 17:41:09 -0500

    Styled header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,10 @@
-
+import { ThemeToggle } from "./ThemeToggle";
 
 export function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="flex items-center justify-between bg-white py-2 pe-10 ps-7 dark:bg-slate-900 dark:text-white">
+      <h1 className="text-3xl font-semibold dark:text-white "> Know Urself</h1>
+      <ThemeToggle />
+    </header>
+  );
 }
-
-
-
-    
-
-
