f6cae696f9a93b55f0684d645e3c2773ad89e58f
Christopher Forti <christopher.forti.79@gmail.com>
2023-11-11 17:40:54 -0500

    Removed unused code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import { Content } from "./components/Content";
 import { Header } from "./components/Header";
-import { Container } from "./components/Container";
-import { useState } from "react";
-import { ThemeToggle } from "./components/ThemeToggle";
 
 function App() {
-  const state = useState("default");
-  const text = state[0];
-  const setText = state[1];
-  function handleClick() {
-    setText("You pushed me");
-  }
-
   return (
     <>
-      <h1 className="text-7xl dark:text-white">
-        My people are destroyed for lack of knowledge
-      </h1>
-      <ThemeToggle />
+      <Header />
     </>
   );
 }
