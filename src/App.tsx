ea322567ddb05398aee32869510c5a2dab6d86e8
Christopher Forti <christopher.forti.79@gmail.com>
2024-05-28 21:04:24 -0400

    Started making initial page, and constructing components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import "./App.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+import { Content } from "./components/Content";
+import { Header } from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [choice, setChoice] = useState(Math.floor(Math.random() * 4));
 
   return (
     <>
-      <button>x</button>
+      <Header />
+      <div className={styles.mainContainer}>
+        <Sidebar />
+        <Content />
+      </div>
     </>
   );
 }
