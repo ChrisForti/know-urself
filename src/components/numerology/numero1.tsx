d381adced705da33de5647de9176ddbb6f3d1679
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-04 18:56:24 -0400

    Content for number 1 started.

---
+++ src/components/numerology/numero1.tsx
@@ -0,0 +1,24 @@
+import styles from "./numero1.module.css";
+
+export function numero1() {
+  return (
+    <div className={styles.one}>
+      <h1>What Does Destiny Number 1 Mean?</h1>
+      <em>Creativity, discipline, independence, leadership…</em>
+      <p>
+        Creativity, discipline, independence, leadership… These are traits
+        associated with destiny number 1. With expression number/destiny number
+        1, you are a born leader. Willpower, strength and determination help you
+        achieve leadership positions, gaining success while reaching goals. With
+        destiny number 1, you are the master of your destiny and life path. You
+        will not tolerate people telling you what to do. You dictate to them
+        while being the centre of attention. Your creativity will help you stay
+        on top and you all keep finding new ways to reach goals. Whether they
+        are goals in family, or career — you will excel in any career where you
+        can lead, as number 1. But you need to be careful. You are strong willed
+        as a 1, which may make you seem blunt, selfish and a number of other
+        negatives.
+      </p>
+    </div>
+  );
+}
