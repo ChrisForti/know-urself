20c65b36d45d0327e2cfda5a0786b3db73cce35f
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-07 21:20:35 -0400

    Number 3 refinements.

--- src/components/numerology/Three.tsx
+++ src/components/numerology/Three.tsx
@@ -3,7 +3,7 @@ import styles from "./Three.module.css";
 export function Three() {
   return (
     <div className={styles.three}>
-      <h1>Destiny Number 3 Meaning</h1>
+      <h1>What Does Destiny Number 3 Mean?</h1>
       <p>
         Being a number 3, makes you a true artist and entertainer. Expressing
         your truth in creative, joyful and uplifting ways. Destiny 3s achieve
@@ -15,11 +15,11 @@ export function Three() {
           <li>humorous</li>
           <li>entertaining</li>
         </ul>
-        If you’re destiny number 3, your optimism makes you a joy to be near.
-        You’re so easy to be around. You hide disappointment behind wit, humour,
-        and a perky expression. You’ll never let anyone see you in pain. You
+        If you are destiny number 3, your optimism makes you a joy to be near.
+        Your so easy to be around. You hide disappointment behind wit, humour,
+        and a perky expression. You will never let anyone see you in pain. You
         crave the love of family and friends but you refuse to let anyone know.
-        Luckily people love you. You’re a performer and full of creativity as a
+        Luckily people love you. You are a performer and full of creativity as a
         3.
       </p>
       <em></em>
