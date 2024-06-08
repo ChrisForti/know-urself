7a0f0019fe39079dc8fbeef7551901abac6387f7
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-08 14:22:41 -0400

    Got the info into number 3.

--- src/components/numerology/Three.tsx
+++ src/components/numerology/Three.tsx
@@ -22,10 +22,30 @@ export function Three() {
         Luckily people love you. You are a performer and full of creativity as a
         3.
       </p>
-      <em></em>
-      <p></p>
-      <em></em>
-      <p></p>
+      <em>Picking a Career</em>
+      <p>
+        It goes without saying that 3 Expression Numbers perform best when they
+        have an audience to entertain. But this does not necessarily mean they
+        need to be “performers”. Working in marketing and sales could be a great
+        fit, as could any position where communication (both written or spoken)
+        is required. The creative industries can be ideal places for those with
+        this Expression Number to thrive: Theatre, film or music, and even
+        journalism could be ideal, depending on the rest of the Numerology
+        chart.
+      </p>
+      <em>3's downside</em>
+      <p>
+        When not expressing at their highest, the Destiny Number 3 can be fake,
+        and people-pleasing. Their need for recognition means that these
+        creative people may actually lose touch with their own inner truth,
+        instead “performing” for the crowd. Yet inauthentic creativity can be a
+        death-knell for this Destiny Number. The breadth of creative talents
+        that the Destiny Number 3 can turn their hand to means that they may
+        struggle with sticking power. Flitting from talent to talent, yet never
+        fully developing a single one, means they may fail to really succeed in
+        life. For this reason, developing discipline early on in life will
+        really serve them.
+      </p>
       <em></em>
     </div>
   );
