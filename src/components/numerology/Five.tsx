4018285d89e71204806b0a1396b109bd0c2ffc7b
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-08 18:00:48 -0400

    Started on number 5, but still need to proof read.

---
+++ src/components/numerology/Five.tsx
@@ -0,0 +1,93 @@
+import { Styles } from "./Five.module.css";
+
+export function Five() {
+  return (
+    <div className={Styles.five}>
+      <h1>What Does Destiny Number 5 Mean?</h1>
+      <em>Carefree, adventurous, witty, progressive, enthusiastic…</em>
+      <p>
+        Destiny number 5 is all about communication. You’re an incredible
+        communicator, talking with old people and new people in different ways.
+        You need a variety of people in your life to be happy. You can use your
+        knack for communication to make new friends as you go through life.
+        You’ll be making new friends a lot, as you tend to outgrow your old ones
+        most of the time. It’s because you’re so restless as a destiny number 5.
+        You need to travel, explore new places and new cultures. You have no
+        time for people holding you back. You adore change and newness, number
+        5. You thrive on it. You get bored too quickly and this keeps you
+        occupied. Your attention span is too low to stay in one place. This
+        sounds like a downer, but is it really, number 5? It makes you
+        progressive, adaptive and adventurous. You can have fun anywhere and
+        handle whatever life throws at you. But try not to be too critical of
+        whatever comes your way. That’s something you need to work on as a
+        number 5. Also, try to work on your need for escapism—you should be
+        doing things for enjoyment, not to run away. Instead of running away
+        from life number 5, maybe you could start a changeable career. It’s
+        something that could keep your short attention span under control, keep
+        you busy, but keep you anchored. There’s no room for escapism now. To
+        find a career that’s truly aligned with your highest good look at every
+        number in your numerology chart. Not just your destiny number. You soul
+        urge, expression, life path and personality numbers all influence your
+        path through life.
+      </p>
+      <h2>Adventure as a 5</h2>
+      <em>your an adventurer, an explorer, right to the core.</em>
+      <p>
+        Everyone is born with some adventure in them, but you more than any
+        other, destiny number 5. Routine? Never. Business? Only if it’s daring
+        and changeable. Aim for constant progress, constant change in the
+        universe, to thrive, number 5. Fives have the confidence to take to the
+        road with enthusiasm, hunting for new information. New people. You
+        travel, not hunting wealth or intellect or to reach any goal. You just
+        want a change of environment. New conditions. New times and
+        eccentricity. As a 5,
+      </p>
+      <em>Changeable, Adventurous Worker</em>
+      <p>
+        But how does one survive in this wealth-driven, job-driven world, off of
+        just travelling? There’s a certain romance about never staying in one
+        place, living life to the fullest. But eventually, the world catches up,
+        and you need to survive unless you’re already wealthy. So, to keep the
+        adventure alive in a career environment, you need constant change. Your
+        attention span is so short. Fives make wonderful writers and
+        journalists, as there’s always a new story to tell. A 5 can make an
+        incredible artist, always up for a new medium, a new piece. A new trip
+        to take, to paint a new landscape or a new group of people. Or you can
+        take a more stable route. Those with the destiny number 5 make excellent
+        teachers. There’s always new intelligence to share, new wit to spread
+        among the class. And a new class every year to keep the change you so
+        desire. But if you really want a career adventure, you could even go
+        into politics. The world needs a progressive leader, and your natural
+        wit will help you succeed. The pursuit of a career in politics is enough
+        adventure for a lifetime.
+      </p>
+      <em>Addiction</em>
+      <p>
+        The destiny number 5 brings stress with it, though. Your constant need
+        to go in a new direction can have an impact on you that’s not always a
+        good one. And a 5 has a naturally addictive personality. Perhaps your
+        addiction is to power. Or exploration. Or escapism and running away.
+        There are many aspects of life where addiction plays its wicked games.
+        So you need to be careful. Surround yourself with people who match your
+        character, but don’t enable your flaws. People you can share your
+        feelings with, who suit your lifestyle. Your changeable nature as a 5
+        means you’ll need people who change enough to stay interesting to you.
+        The right people around you can make possible future-addictions into
+        things that comfort you, but no more. So you’ll enjoy the power, but
+        know when to take a step back. You won’t crave it, only enjoy it. You
+        want things to spark excitement; you want your responsibilities to spark
+        joy. But you don’t want to become addicted to what they bring, because
+        what if it all comes crashing down? You can no longer travel. You’re
+        forced to retire. Or worse, you fail in your career before it starts.
+        Don’t let yourself get hooked on fickle things, because they’ll destroy
+        you. Don’t let yourself get addicted to anything to ease the stresses of
+        life, either! Everything in life has the potential to become a problem.
+        Even the adventure you crave so much. Have your loved ones and family
+        members ready to steady you. Others will be your salvation, even if it’s
+        in your nature to leave them behind. Change the way you see the world,
+        and your addictive personality will quieten. Instead, you’ll become
+        addicted to the people in your life, not the thrills that can hurt you.
+      </p>
+    </div>
+  );
+}
