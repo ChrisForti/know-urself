ceb14273d41103c1ba3f0ea4a3077102c56a392e
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-09 19:34:21 -0400

    Content to number 8.

--- src/components/numerology/Eight.tsx
+++ src/components/numerology/Eight.tsx
@@ -1,5 +1,98 @@
 import styles from "./Eight.module.css";
 
 export function Eight() {
-  return <div className={styles.eight}>Eight</div>;
+  return (
+    <div className={styles.eight}>
+      <h1>What Does Destiny Number 8 Mean?</h1>
+      <em>
+        Great motivator, determined, leadership ability, hard-working, powerful,
+        organized, successful…
+      </em>
+      <p>
+        Destiny number 8, you’re a success. Your natural ability to lead and
+        your commitment to hard work grant you this. Destiny number 8 people
+        make near-perfect leaders because of their responsible nature, but
+        almost is the operative word here. Go too far and you get competitive.
+        And not only in the friendly competition way. You’re likely to win any
+        competition because you’re organized and you put in the work, destiny
+        number 8. However, your distrustful and intolerant nature may make it
+        harder for you to excel when the competition comes. Balance is tricky
+        for you, with an 8 destiny. Keeping work ambition and family challenges
+        separate. But your stubbornness and refusal to give in should help you
+        deal with stress. As you progress, you learn to deal. Proper navigation
+        through each adventure is key. You may also find that if your soul urge
+        or life path number is a 1 then your ability to lead will be magnified.
+        These numbers blend with your destiny number to create your unique soul
+        blueprint and life potential. To discover how your numbers blend
+        together, click here for a free numerology reading. It covers your core
+        numbers and explores the relationship between them.
+      </p>
+      <h2>A Career in Authority</h2>
+      <p>
+        You have authority over so much, destiny number 8. You’re a leader. This
+        makes careers hard for you, as you need to be on top. You need to be in
+        power, or it probably won’t work out. Eights tend to be entrepreneurs
+        and risk-takers in their careers. Forgoing that, you’re suited to a
+        career in military, government, finance or banking—so long as you can
+        work your way to the top. You’re powerful and need a career that’ll
+        reward you for your hard work. People with destiny number 8 excel beyond
+        any other destiny number. Your pursuit of power drives you past the
+        boundaries. Your mission is to get through obstacles that stand in your
+        way to the top, and stay there. That’s why, in most cases, you won’t be
+        suited to a career that doesn’t put you in a leadership position. Your
+        intolerance is clear. Even if you can’t get a job with true power,
+        eights make excellent managers. As a destiny number eight, you have the
+        organization and leadership qualities for it. Under a circumstance where
+        you do find comfort in a power-free job, you’re the perfect employee,
+        destiny number 8. You’re a great motivator of your peers and a
+        hand-working mastermind. You’ll probably climb your way to the top,
+        intentional or not. But you need to be very careful with your ambitions
+        here. It can put a strain on your relationship as you neglect family to
+        focus on work. Since failures aren’t your forté, you should aim for a
+        balance. Focus on what matters, the loved ones who fill your heart and
+        soul, and don’t let work influence you to turn away from them.
+      </p>
+      <h2>Determination to Succeed</h2>
+      <p>
+        Individuality is a gift, destiny number 8. And being the type of
+        individual you are, a hard-working one focused on the liberation of
+        yourself, can be a challenge. Why do people want to be on top? For
+        attention? To do good and reap a reward? Or is it for freedom? The
+        freedom that respected leaders have to be themselves. It’s one of the
+        benefits of being on top. It’s most likely your darker traits that push
+        you to be independent. You’re distrustful of others. Paranoid that
+        everyone’s out to get you. Suspicious of the competition. You need to be
+        free and operate alone. Being successful at work will help you better
+        yourself, destiny number 8. Once you’re on top of your game, there’s no
+        need to be suspicious that anyone’s trying to beat you to it. Just be
+        careful not to let your conservative beliefs and unemotional nature get
+        in the way once you hit the top. Your determination to succeed may also
+        help with your tendency to neglect relationships, number 8. Once you
+        succeed in business, you’re out of the race. Giving you plenty of time
+        to mend broken relationships and start new ones. If you’re struggling
+        with love relationships, people, life and family, just hold on to the
+        need to succeed. You can succeed in more than just work, you know. Being
+        successful in a career brings money. Being successful in relationships
+        brings another kind of wealth to your soul, number 8.
+      </p>
+      <h2>Distrust</h2>
+      <p>
+        Sadly number 8, it’s not just work that can take a hit because of your
+        distrust. You’re just a suspicious and skeptical person overall. You
+        need to be careful with this in every aspect of your life, as an 8. Say
+        you’ve achieved your greatest career goal, you’re married and you have a
+        family. Now what, number 8? You’re successful in everything in life.
+        What’s left to strive for? Without something to work towards, your
+        distrustful side as an 8 may take over. You could get paranoid and start
+        thinking it’s all too good to be true. That your company will fail. That
+        your spouse will leave you. Remember that you’ve had a lot of luck and
+        good fortune to get where you are. But on top of that, you’ve worked for
+        it. You’ve walked the right paths, utilized honesty, developed wisdom
+        and a better character within yourself. Even if anyone were trying to
+        take away your work or your family, you wouldn’t let them. You are the
+        master of your purpose. Don’t let your distrust of people take away your
+        successes, destiny number 8.
+      </p>
+    </div>
+  );
 }
