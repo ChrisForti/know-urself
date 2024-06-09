ad6c9e8e2236a1c2e29e6d9bc3425f98a63b0863
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-09 19:41:12 -0400

    Content for number nine.

--- src/components/numerology/Nine.tsx
+++ src/components/numerology/Nine.tsx
@@ -1,5 +1,94 @@
 import styles from "./Nine.module.css";
 
 export function Nine() {
-  return <div className={styles.nine}>Nine</div>;
+  return (
+    <div className={styles.nine}>
+      <h1>What Does Destiny Number 9 Mean?</h1>
+      <em>Artistic, generous, idealistic, poetic, visionary…</em>
+      <p>
+        But there are some dark words, too, destiny number 9. Depressive.
+        Gullible. Impatient. Easily disappointed. Destiny number 9 people are
+        fascinating. Destiny number 9 people are philanthropists. Charitable.
+        Appreciators of the artistic and the beautiful. Perhaps you can
+        appreciate beauty and art so much because you’re overly sensitive—a
+        challenging attribute that can aid your abilities as a visionary, number
+        9. As a visionary, you want to beautify everything. The world. Others.
+        Maybe yourself. You want to achieve a lot in this lifetime through
+        beauty, art and a variety of activities you can indulge in. Your main
+        flaw is that you’re unrealistic about doing it, number 9. You’re
+        impatient and want everything now. This is unrealistic and thinking this
+        way can only hurt you. You’re easily disappointed and sensitive. And you
+        don’t do well for long periods without variety or quality. You refuse to
+        wait for things to happen, and you refuse to wait for other people to
+        catch up to you. If they’re dull, slow or less intelligent, you
+        disregard them. They’re simply in the way, number 9. You can learn to be
+        tolerant, destiny number 9. You’d do well to learn to wait for people
+        and forgive the ignorant. To see how you can balance your weaknesses,
+        look at the strengths, talents and gifts elsewhere in your numerology
+        chart. You have 5 core numerology numbers which combine and blend
+        together to create your unique soul blueprint. To learn what these
+        numbers are, and discover how they blend together, click here for a free
+        numerology reading. It will calculate your full numerology profile based
+        on your name and date of birth.
+      </p>
+      <h2>Selfishness</h2>
+      <p>
+        You have selflessness that most people lack. Selective humanity that
+        knows your lifestyle is not universal. You respect those with less and
+        treat them with kindness. You care for them when it’s within your means,
+        number 9. Parts of your personality are beautiful. Kindness is one of
+        your better characteristics and selflessness. Even when they don’t have
+        much, nines take chances and give what little they have to those with
+        bigger hardships. Material possessions aren’t that important to you and
+        others need the gifts, number 9. Yet you have a side of you that deems
+        many people aren’t worthy of your time. You’ll only spend energy on
+        those you think are up to your standards, number 9. That’s one of the
+        challenges you’ll face in life and it might put you in a difficult
+        position or two. Take note of the opportunities you give to those in
+        need. The strength you offer. You don’t seek fame or worship from
+        helping. You get no benefit. You’re kind and selfless. So why is it that
+        in other areas of life you’re selfish, and deem almost nothing as worth
+        your time?
+      </p>
+      <h2>Sensitivity</h2>
+      <p>
+        Answering the question of your paradoxical selfless/selfish personality
+        is something only you can do. I only offer theories. Perhaps your
+        beautiful presence, the side of you that helps those without fortune,
+        comes from your sensitivity. You want to beautify everything, so you’re
+        sensitive to seeing beauty in everyone as a 9. You have the power to let
+        people see it within themselves. You’re a visionary and want to create
+        the world that you see in your mind’s eye. So you take action and help
+        those you think can help you achieve aspects of it, number 9. As a
+        sensitive person, you can be overly sensitive in your pursuit of
+        perfection. This can not only hurt you, but others. Proceed with
+        caution, 9. You need to take responsibility for any times when you react
+        in an overly-sensitive manner. If you don’t step up and act with
+        honesty, it could cause a problem. Instead of taking everything to heart
+        and falling into a depressive episode, focus on the better parts of the
+        word. Sensitivity. Think of the feeling you get when you help someone.
+        Your intuition about a group who could bring beauty to the world. Then
+        do your best to control your temper, think about your goal of beauty,
+        and proceed. Be aware, your sensitivity can also make you impressionable
+        and gullible. As a 9 who’s easily disappointed, these things could bring
+        up emotions that aren’t healthy to have.
+      </p>
+      <h2>Impatience</h2>
+      <p>
+        Although you wish to beautify the world, you may have other goals too.
+        And you’re idealistic about them, sometimes to an extent that’s
+        unrealistic and impractical. Your goals may even extend past wanting to
+        beautify the world, but instead wanting the world itself. Your
+        selfishness can creep out and override your past generosity, number 9.
+        You want what you want now and if you don’t get it you’ll fall into a
+        mental state that’s undesirable. This impatience may even apply to your
+        selfless actions, too. You want those receiving your generosity to get
+        what they need at once. You need to learn to wait for things, and even
+        people. Your impatience extends to people, too. Around the ones you
+        don’t feel are worthy of your time. You’re tolerant to an extent, but
+        need to learn to apply it to situations where you’re being arrogant
+        around people you see as lesser, number 9.
+      </p>
+    </div>
+  );
 }
