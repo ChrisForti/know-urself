8063d9471f26b1c666703d56861540d72e5d423d
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-10 20:28:54 -0400

    Proof read number 9, and should apply this syntax to the rest of the files.

--- src/components/numerology/Nine.tsx
+++ src/components/numerology/Nine.tsx
@@ -35,7 +35,7 @@ export function Nine() {
         </p>
       </p>
       <h2>Selfishness</h2>
-      <p>
+      <span>
         <p>
           You have selflessness that most people lack. Selective humanity that
           knows your lifestyle is not universal. You respect those with less and
@@ -47,53 +47,68 @@ export function Nine() {
           important to you and others need the gifts, number 9. Yet you have a
           side of you that deems many people are not worthy of your time.
         </p>
-        You will only spend energy on those you think are up to your standards,
-        number 9. That is one of the challenges you will face in life, and it
-        might put you in a difficult position or two. Take note of the
-        opportunities you give to those in need. The strength you offer, is you
-        do not seek fame or worship from helping. You get no benefit, and your
-        kind and selfless. So why is it that in other areas of life you are
-        selfish, and deem almost nothing as worth your time?
-      </p>
+        <p>
+          You will only spend energy on those you think are up to your
+          standards, number 9. That is one of the challenges you will face in
+          life, and it might put you in a difficult position or two. Take note
+          of the opportunities you give to those in need. The strength you
+          offer, is you do not seek fame or worship from helping. You get no
+          benefit, and your kind and selfless. So why is it that in other areas
+          of life you are selfish, and deem almost nothing as worth your time?
+        </p>
+      </span>
       <h2>Sensitivity</h2>
-      <p>
+      <em>
         Answering the question of your paradoxical selfless/selfish personality
-        is something only you can do. I only offer theories. Perhaps your
-        beautiful presence, the side of you that helps those without fortune,
-        comes from your sensitivity. You want to beautify everything, so you’re
-        sensitive to seeing beauty in everyone as a 9. You have the power to let
-        people see it within themselves. You’re a visionary and want to create
-        the world that you see in your mind’s eye. So you take action and help
-        those you think can help you achieve aspects of it, number 9. As a
-        sensitive person, you can be overly sensitive in your pursuit of
-        perfection. This can not only hurt you, but others. Proceed with
-        caution, 9. You need to take responsibility for any times when you react
-        in an overly-sensitive manner. If you don’t step up and act with
-        honesty, it could cause a problem. Instead of taking everything to heart
-        and falling into a depressive episode, focus on the better parts of the
-        word. Sensitivity. Think of the feeling you get when you help someone.
-        Your intuition about a group who could bring beauty to the world. Then
-        do your best to control your temper, think about your goal of beauty,
-        and proceed. Be aware, your sensitivity can also make you impressionable
-        and gullible. As a 9 who’s easily disappointed, these things could bring
-        up emotions that aren’t healthy to have.
-      </p>
+        is something only you can do.
+      </em>
+      <span>
+        <p>
+          I only offer theories. Perhaps your beautiful presence, the side of
+          you that helps those without fortune, comes from your sensitivity. You
+          want to beautify everything, so you are sensitive to seeing beauty in
+          everyone as a 9. You have the power to let people see it within
+          themselves. Your a visionary and want to create the world that you see
+          in your minds eye. So you take action and help those you think can
+          help you achieve aspects of it, number 9. As a sensitive person, you
+          can be overly sensitive in your pursuit of perfection. This can not
+          only hurt you, but others.
+        </p>
+        <em>Proceed with caution, 9.</em>
+        <p>
+          You need to take responsibility for any times when you react in an
+          overly-sensitive manner. If you do not step up and act with honesty,
+          it could cause a problem. Instead of taking everything to heart and
+          falling into a depressive episode, focus on the better parts of the
+          word. Sensitivity. Think of the feeling you get when you help someone.
+          Your intuition about a group who could bring beauty to the world. Then
+          do your best to control your temper, think about your goal of beauty,
+          and proceed. Be aware, your sensitivity can also make you
+          impressionable and gullible. As a 9 who is easily disappointed, these
+          things could bring up emotions that are not healthy to have.
+        </p>
+      </span>
       <h2>Impatience</h2>
-      <p>
-        Although you wish to beautify the world, you may have other goals too.
-        And you’re idealistic about them, sometimes to an extent that’s
-        unrealistic and impractical. Your goals may even extend past wanting to
-        beautify the world, but instead wanting the world itself. Your
-        selfishness can creep out and override your past generosity, number 9.
-        You want what you want now and if you don’t get it you’ll fall into a
-        mental state that’s undesirable. This impatience may even apply to your
-        selfless actions, too. You want those receiving your generosity to get
-        what they need at once. You need to learn to wait for things, and even
-        people. Your impatience extends to people, too. Around the ones you
-        don’t feel are worthy of your time. You’re tolerant to an extent, but
-        need to learn to apply it to situations where you’re being arrogant
-        around people you see as lesser, number 9.
-      </p>
+      <span>
+        <p>
+          Although you wish to beautify the world, you may have other goals too.
+          And you are idealistic about them, sometimes to an extent that is
+          unrealistic and impractical. Your goals may even extend past wanting
+          to beautify the world, but instead wanting the world itself. Your
+          selfishness can creep out and override your past generosity, number 9.
+          You want what you want now and if you do not get it, you will fall
+          into a mental state that is undesirable. This impatience may even
+          apply to your selfless actions, too. You want those receiving your
+          generosity to get what they need at once.
+        </p>
+        <p>
+          You need to learn to wait for things, and even people. Your impatience
+          extends to people, too. Around the ones you don not feel are worthy of
+          your time. You are tolerant to an extent, but need to learn to apply
+          it to situations where your being arrogant around people you see as
+          lesser, number 9.
+        </p>
+      </span>
     </div>
   );
 }
