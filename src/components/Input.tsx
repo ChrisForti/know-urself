36995ed4cab8f07dbfa5725f865a48111a7ab6e3
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-15 19:11:12 -0400

    Instructions for the input box.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,6 +14,7 @@ export function Input({ setChoice }: InputProps) {
   }
   return (
     <form className={styles.input} onSubmit={handleSubmit}>
+      <div>Enter full name</div>
       <input
         type="text"
         value={value}
