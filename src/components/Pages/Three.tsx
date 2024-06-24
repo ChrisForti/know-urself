3075d532244756902055fca62d5373445888d341
Christopher Forti <christopher.forti.79@gmail.com>
2024-06-24 19:29:18 -0400

    Updated number 3's content to match the other pages.

--- src/components/Pages/Three.tsx
+++ src/components/Pages/Three.tsx
@@ -4,23 +4,15 @@ export function Three() {
   return (
     <div className={styles.main}>
       <h1>What Does Destiny Number 3 Mean for?</h1>
+      <em>Optimistic, entertaining, humorous, romantis, creative.</em>
       <p>
         Being a number 3, makes you a true artist and entertainer. Expressing
-        your truth in creative, joyful and uplifting ways. Destiny 3s achieve
-        their aims in life by being:
-        <ul>
-          <li>Optimistic</li>
-          <li>creative</li>
-          <li>romantic</li>
-          <li>humorous</li>
-          <li>entertaining</li>
-        </ul>
-        If you are destiny number 3, your optimism makes you a joy to be near.
-        Your so easy to be around. You hide disappointment behind wit, humour,
-        and a perky expression. You will never let anyone see you in pain. You
-        crave the love of family and friends but you refuse to let anyone know.
-        Luckily people love you. You are a performer and full of creativity as a
-        3.
+        your truth in creative, joyful and uplifting ways. If you are destiny
+        number 3, your optimism makes you a joy to be near. Your so easy to be
+        around. You hide disappointment behind wit, humour, and a perky
+        expression. You will never let anyone see you in pain. You crave the
+        love of family and friends but you refuse to let anyone know. Luckily
+        people love you. You are a performer and full of creativity as a 3.
       </p>
       <h2>Picking a Career</h2>
       <p>
