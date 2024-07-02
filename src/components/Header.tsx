f54f4716fc956c796adf4289b5d7bd1ab232a883
Christopher Forti <christopher.forti.79@gmail.com>
2024-07-02 18:34:55 -0400

    Changed header back to know yourself.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <h3 className={styles.h3}>Who are you really</h3>
+      <h3 className={styles.h3}>Know yourself</h3>
     </header>
   );
 }
