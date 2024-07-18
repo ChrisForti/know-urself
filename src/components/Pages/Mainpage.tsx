4aa890990c6a3b32ed7d80632cf3e1d36935f148
Christopher Forti <christopher.forti.79@gmail.com>
2024-07-18 19:15:07 -0400

    CHanged header to see if deployment is working.

--- src/components/Pages/Mainpage.tsx
+++ src/components/Pages/Mainpage.tsx
@@ -8,7 +8,7 @@ import { Table } from "./Table";
 export function Mainpage() {
   return (
     <div className={styles.main}>
-      <h1>Main Page</h1>
+      <h1>Theory page</h1>
       <p>
         Welcome to our Know youself, where we delve into the ancient teachings
         of Pythagoras and unlock the secrets of your destiny through the power
